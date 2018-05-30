37a8209746ce1cd8d13110410544f86fd11a7c93
Eric Williams <williams.eric.thomas@gmail.com>
2018-05-30 00:39:39 +0000

    added automatic navbar addition via javascript.

---
+++ navbar.js
@@ -0,0 +1,13 @@
+function navbar() {
+    var nav = \
+        "<tr>\
+	        <th colspan=2>Navigation</th>\
+		</tr>\
+		<tr>\
+			<td><a href=\"dnd-roller.html\">D&D 5e Stat Roller</a></td>\
+			<td><a href=\"about.html\">About</a></td>\
+			<td><a href=\"excellence.html\">Title</a></td>\
+		</tr>";
+
+    document.getElementByID("navbar").innerHTML = nav;
+}
