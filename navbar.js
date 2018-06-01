b538ebd2e59be463dfb48b5adb2c6a5cc6af19be
Eric Williams <williams.eric.thomas@gmail.com>
2018-06-01 23:15:26 +0000

    more testing

--- navbar.js
+++ navbar.js
@@ -1,12 +1,12 @@
 function navbar() {
-    /*var nav = "<tr>\
+    var nav = "<tr>\
 	        <th colspan=2>Navigation</th>\
 		</tr>\
 		<tr>\
 			<td><a href=\"dnd-roller.html\">D&D 5e Stat Roller</a></td>\
 			<td><a href=\"about.html\">About</a></td>\
 			<td><a href=\"excellence.html\">Title</a></td>\
-		</tr>"*/
+		</tr>";
 
-    document.getElementById("navbar").innerHTML = "TEST";
+    document.getElementById("navbar").innerHTML = nav;
 }
