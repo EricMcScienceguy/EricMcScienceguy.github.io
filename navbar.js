42cdff2d0eb12ae608a87f39bff5e2113040aa6d
Eric Williams <williams.eric.thomas@gmail.com>
2018-06-01 23:07:36 +0000

    testing the navbar to see what's wrong

--- navbar.js
+++ navbar.js
@@ -1,14 +1,14 @@
 function navbar() {
-    var nav = "<tr>\
+    /*var nav = "<tr>\
 	        <th colspan=2>Navigation</th>\
 		</tr>\
 		<tr>\
 			<td><a href=\"dnd-roller.html\">D&D 5e Stat Roller</a></td>\
 			<td><a href=\"about.html\">About</a></td>\
 			<td><a href=\"excellence.html\">Title</a></td>\
-		</tr>";
+		</tr>"*/
 
-    document.getElementById("navbar").innerHTML = nav;
+    document.getElementById("navbar").innerHTML = "TEST";
 }
 
 navbar();
