596f792244923910d0dbff68b0399776dd393a9d
Eric Williams <williams.eric.thomas@gmail.com>
2018-06-03 02:33:35 +0000

    - made the navbar thing work procedurally, rather than as a hardcoded string  - made it search for class, rather than by id (makes css easier)  - made the title of the navbar matchup with the total number of cells in it

--- navbar.js
+++ navbar.js
@@ -1,12 +1,44 @@
-function navbar() {
-    var nav = "<tr>\
-	        <th colspan=2>Navigation</th>\
-		</tr>\
-		<tr>\
-			<td><a href=\"dnd-roller.html\">D&D 5e Stat Roller</a></td>\
-			<td><a href=\"about.html\">About</a></td>\
-			<td><a href=\"excellence.html\">Title</a></td>\
-		</tr>";
+/*eslint-disable no-unused-vars */
 
-    document.getElementById("navbar").innerHTML = nav;
+// Object that stores the name and url of a page together
+function Page(name, url) {
+    this.name = name; // name of the page to be displayed
+    this.url = url; // url of the page to be displayed
+    
+    // Return the name of the page
+    this.getName = function () {
+        return this.name;
+    };
+    
+    // Return the url of the page
+    this.getUrl = function() {
+        return this.url;
+    };
+}
+
+// Generate the navbar. Most of the function goes to generating the string to insert
+function navbar() {
+    // Initialize the pages
+    var homePage = new Page("Home", "index.html");
+    var statPage = new Page("D&D 5e Stat Roller", "dnd-roller.html");
+    var aboutPage = new Page("About", "about.html");
+    var titlePage = new Page("<i>The</i> Title", "excellence.html");
+    
+    // Store the pages, determine the order of them
+    var pages = [homePage, aboutPage, titlePage, statPage];
+    var n = pages.length; // number of pages
+    
+    var nav = "<tr>\
+            <th colspan=" + n + ">Navigation</th>\
+        </tr>\
+        <tr>";
+    
+    // Add each page into the navbar string
+    var p;
+    for (p in pages) {
+        nav += "<td><a href=\"" + p.getUrl() + "\">" + p.getName() + "</a></td>";
+    }
+    nav += "</tr>"; // throw in the row end
+    
+    document.getElementsByClassName("navbar")[0].innerHTML = nav; // push that baby to the page
 }
